5d1e8179650c7b12b9cca31e4c451a3f54a583cd
shahnawazknpr <120628980+shahnawazknpr@users.noreply.github.com>
2022-12-15 00:03:39 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourite')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬Kale Spinach & Rocket Smoothie')
