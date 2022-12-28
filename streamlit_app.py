df4360c4f3292bcc84faf31bf34c137b4fbfa75d
shahnawazknpr <120628980+shahnawazknpr@users.noreply.github.com>
2022-12-27 23:36:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,24 @@ streamlit.dataframe(fruits_to_show)
 
 #New section tp display fruityvice API responce
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+    try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+
+        if not fruit_choice:
+          streamlit.error("Please select a fruit get the information.")
+        else:
+        #import requests
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+        # write your own comment -what does the next line do? 
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        # write your own comment - what does this do?
+        streamlit.dataframe(fruityvice_normalized)
+     
+    exception URLError as e:
+        streamlit.error()
 
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
 # stop execution 
 streamlit.stop()
